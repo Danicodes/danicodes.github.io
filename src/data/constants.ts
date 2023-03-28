98b694d59b34b2f9e6316355259165bbc782755e
danicodes <danicodes94@gmail.com>
2023-03-28 00:05:08 -0400

    Revamping portfolio in Asto dev

---
+++ src/data/constants.ts
@@ -0,0 +1,25 @@
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about",
+	},
+	{
+		title: "Blog",
+		path: "/posts",
+	},
+];
+
+// ! Remember to add your own socials
+export const SOCIAL_LINKS = {
+	github: "https://github.com/danicodes",
+	twitter: "https://www.twitter.com/girlwithbox",
+	email: "@protonmail.com",
+};
+
+export const About = {
+	description: "Hi I'm Dani!"
+};
